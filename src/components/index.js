846ae919e7c49e2c767b6fc1f12d35237b14c760
vicelemon1841 <lsk18415@naver.com>
2025-10-15 17:16:32 +0900

    메인 검색 창에서 달력 추가

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
 export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 export { default as Button } from './Button';
-export { default as HotelCard } from './HotelCard';
-export { default as HotelFilters } from './HotelFilters';
+export { default as HotelCard } from './hotelSearch/HotelCard';
+export { default as HotelFilters } from './hotelSearch/HotelFilters';
 
 // Admin Components
 export { default as AdminLayout } from './admin/AdminLayout';
