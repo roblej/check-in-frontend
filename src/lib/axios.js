84e9a93c0400666fb0e39218f43479f06690a035
junyongseong <seongyongjun1160@gmail.com>
2025-10-22 17:15:03 +0900

    팀 컨벤션 통일을 위한 환경변수 제거 후 api 모듈 통일

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Axios 인스턴스 생성
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8888/api",
+  baseURL:  "http://localhost:8888/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
