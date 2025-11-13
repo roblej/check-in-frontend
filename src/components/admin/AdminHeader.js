89614020ff3a5b866af09568a7ebbdd79183331a
vicelemon1841 <lsk18415@naver.com>
2025-11-13 10:23:48 +0900

    Update AdminHeader.js

--- src/components/admin/AdminHeader.js
+++ src/components/admin/AdminHeader.js
@@ -17,7 +17,7 @@ const AdminHeader = ({ onMenuClick }) => {
   
   const handleLogout = () => {
     // 고객 스토어 초기화
-    axios.get('/login/logout');
+    axiosInstance.get('/login/logout');
     resetAccessToken("");
     setInlogged(false);
     
